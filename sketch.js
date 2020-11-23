b7bcd67cd56b0bec24e415a4db297b9bf4cafd7b
rashmi <rashmi.r@whitehatjr.com>
2020-11-23 12:58:23 +0530

    Add

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,300,170)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -57,6 +58,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+platform.display()
     bird.display();
 }
